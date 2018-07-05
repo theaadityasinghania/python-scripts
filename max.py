3c9680d34c25215973dbe24fe227a433b3867add
theaadityasinghania <theaadityasinghania@gmail.com>
2018-07-05 11:47:45 +0530

    First file added

---
+++ max.py
@@ -0,0 +1,9 @@
+# Write a program to find the maximum of two numbers stored in variables.
+
+
+x=int(input("Enter the first number here: "))
+y=int(input("Enter the Second number here: "))
+if x>y:
+    print("The greatest number is: ",x)
+else:
+    print ("The greatest number is: ",y)
