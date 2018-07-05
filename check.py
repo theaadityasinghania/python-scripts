d6eede15c0372d7c9bc152355340ac35c7b464f4
theaadityasinghania <theaadityasinghania@gmail.com>
2018-07-05 12:05:42 +0530

    Third file added

---
+++ check.py
@@ -0,0 +1,11 @@
+# Program to check whether a number is negative, positive or zero.
+# Printing out “negative”, “positive”, or “zero”.
+
+
+x=int(input("Enter any number here: "))
+if(x>0):
+    print("The number entered is Positive")
+elif(x<0):
+    print("The number entered is Negative")
+else:
+    print("The number entered is Zero")
